cdb84422d9d521d87782136f6ec4d9d383fa2612
chunyu <cyyong95x@gmail.com>
2020-08-20 19:17:51 +0800

    Initial commit

---
+++ SimpleProjAPI.Test/WeatherForecastTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SimpleProjAPI.Controllers;
+using Xunit;
+
+namespace SimpleProjAPI.Test
+{
+    public class WeatherForecastTest
+    {
+        WeatherForecastController _weatherForecastController = new WeatherForecastController();
+
+        [Fact]
+        public void Get_HotWeatherForecast()
+        {
+            //Arrange
+            const string _hotWeather = "Hot";
+
+            // Act
+            var _weatherForecast = _weatherForecastController.Get(_hotWeather);
+
+            // Assert
+            Assert.True(_weatherForecast.Summary == _hotWeather);
+        }
+    }
+}
---
+++ SimpleProjAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleProjAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("{weather}")]
+        public WeatherForecast Get(string weather)
+        {
+            var _weather = Summaries.FirstOrDefault(weatherSummary => weatherSummary.ToLower() == weather.ToLower());
+
+            if (_weather == null)
+            {
+                return null;
+            }
+
+            var _rng = new Random();
+            return new WeatherForecast
+            {
+                Date = DateTime.Now,
+                TemperatureC = _rng.Next(-20, 55),
+                Summary = _weather
+            };
+        }
+    }
+}
